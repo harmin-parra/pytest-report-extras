969934b71da7f7f5739bc56d35189ea9773a8f1f
harmin-parra <harmin.parra@gmail.com>
2025-03-09 21:18:13 +0100

    update

--- src/pytest_report_extras/plugin.py
+++ src/pytest_report_extras/plugin.py
@@ -274,9 +274,9 @@ def pytest_configure(config):
     # Retrieve some options
     fx_html = utils.get_folder(config.getoption("--html", default=None))
     fx_allure = config.getoption("--alluredir", default=None)
-    fx_single_page = config.getoption("--self-contained-html", default=False)
     fx_tms_link = config.getini("extras_tms_link_pattern")
     fx_issue_link = config.getini("extras_issue_link_pattern")
+    fx_single_page = config.getoption("--self-contained-html", default=False)
     utils.check_options(fx_html, fx_allure)
     # Add markers
     config.addinivalue_line("markers", "issues(keys): The list of issue keys to add as links")
